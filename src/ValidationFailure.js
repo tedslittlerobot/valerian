dc053712cbbae495a472e5820438eafc064bd57a
Stefan Horner <shorner@golin.com>
2017-12-08 11:43:18 +0000

    refactor messages to own class

--- src/ValidationFailure.js
+++ src/ValidationFailure.js
@@ -1,48 +1,12 @@
-import _ from 'lodash';
 
 export default class ValidationFailure extends Error {
-  constructor() {
-    super('There were Validation Errors');
+  constructor(errors, ...params) {
+    super('There were validation errors.', ...params);
 
-    this.errors = {};
-    Error.captureStackTrace(this, ValidationFailure);
-
-    this.addMessage = this.addMessage.bind(this);
-    this.hasMessage = this.hasMessage.bind(this);
-    this.hasMessages = this.hasMessages.bind(this);
-    this.first = this.first.bind(this);
-    this.all = this.all.bind(this);
-  }
-
-  addMessage(key, message) {
-    if (!this.errors[key]) {
-      this.errors[key] = [];
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ValidationFailure);
     }
 
-    this.errors[key].push(message);
-  }
-
-  hasMessages(key = null) {
-    if (!key) return _.size(this.errors) > 0;
-
-    return !!this.errors[key];
-  }
-
-  hasMessage(key = null) {
-    return this.hasMessage(key);
-  }
-
-  first(key) {
-    if (!this.errors[key]) return null;
-
-    return this.errors[key][0];
-  }
-
-  all(key = null) {
-    if (!key) return this.errors; // return all errors if no key is supplied
-
-    if (!this.errors[key]) return null;
-
-    return this.errors[key];
+    this.errors = errors;
   }
 }
